fc753e0cf6f4884003b6c2f8771a11dbefc00d9d
Chris Walker <thatamazingweb@gmail.com>
2018-06-14 21:18:43 +0100

    Add pagerduty client

---
+++ pkg/pagerduty/model.go
@@ -0,0 +1,21 @@
+package pagerduty
+
+type Incidents struct {
+	Incidents []Incident `json:"incidents"`
+}
+
+type Incident struct {
+	ID             string `json:"id"`
+	Title          string `json:"title"`
+	IncidentNumber int    `json:"incident_number"`
+	CreatedAt      string `json:"created_at"`
+	Status         string `json:"status"`
+	//	Assignments      []Assignment      `json:"assignments"`
+	//	Acknowledgements []Acknowledgement `json:"acknowledgements"`
+}
+
+type Assignment struct {
+}
+
+type Acknowledgement struct {
+}
---
+++ pkg/pagerduty/pagerduty.go
@@ -0,0 +1,49 @@
+package pagerduty
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/pkg/errors"
+)
+
+var (
+	pagerDutyURL = "https://api.pagerduty.com/incidents"
+)
+
+type PagerDutyClient struct {
+	httpClient *http.Client
+	token      string
+}
+
+func NewPagerDutyClient(token string) *PagerDutyClient {
+	return &PagerDutyClient{
+		token:      token,
+		httpClient: &http.Client{},
+	}
+}
+
+func (p *PagerDutyClient) GetIncidents() ([]Incident, error) {
+	req, err := http.NewRequest("GET", pagerDutyURL, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not create 'GET incidents' request")
+	}
+	req.Header.Add("Authorization", "Token token="+p.token)
+
+	resp, err := p.httpClient.Do(req)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not retrieve incidents")
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	incidents := Incidents{}
+	err = json.Unmarshal(body, &incidents)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not unmarshal incidents")
+	}
+
+	return incidents.Incidents, nil
+}
