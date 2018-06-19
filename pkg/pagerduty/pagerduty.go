118440621ecdad0eec9acfcd72f945466c78af17
Chris Walker <chris.walker@karhoo.com>
2018-06-19 15:36:52 +0100

    Add simple `since` query to request

--- pkg/pagerduty/pagerduty.go
+++ pkg/pagerduty/pagerduty.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"time"
 
 	"github.com/pkg/errors"
 )
@@ -30,6 +31,13 @@ func (p *Client) GetIncidents() ([]Incident, error) {
 		return nil, errors.Wrap(err, "could not create 'GET incidents' request")
 	}
 	req.Header.Add("Authorization", "Token token="+p.token)
+	req.Header.Add("Accept", "application/vnd.pagerduty+json;version=2")
+
+	t := getTimeYesterday()
+
+	q := req.URL.Query()
+	q.Add("since", t.Format(time.RFC3339))
+	req.URL.RawQuery = q.Encode()
 
 	resp, err := p.httpClient.Do(req)
 	if err != nil {
@@ -46,4 +54,13 @@ func (p *Client) GetIncidents() ([]Incident, error) {
 	}
 
 	return incidents.Incidents, nil
+}
+
+func getTimeYesterday() time.Time {
+	t := time.Now()
+
+	hrs := t.Hour() + 7 // Takes number of hours from now, until 5pm (approx) previous day
+	prevTime := t.Add(time.Duration(-hrs) * time.Hour)
+
+	return prevTime
 }
